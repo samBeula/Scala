4247d4fb2d32fe9c5b371d5f592d69d2295ffd9a
BeulaSamantha <95486339+samBeula@users.noreply.github.com>
2023-03-25 10:04:01 +0530

    Place hode notation in scala

---
+++ PlaceHolderNotation.scala
@@ -0,0 +1,14 @@
+object PlaceHolderNotation {
+  def main(args: Array[String]): Unit = {
+
+    val a = 10
+    val b = 20
+    val v1 = testM(_ + _, a, b, 30.0)
+    println(s"===>$v1")
+  }
+
+  def testM(highFunction: (Int, Int) => Int, a: Int, b: Int, d: Double): Double = {
+    highFunction(a, b) * d
+  }
+
+}
